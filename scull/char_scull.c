737320fbde96353d51baf94acf7e29da08afdef7
Ketan Arlulkar <ketan.arlulkar@hotmail.com>
2015-12-24 07:50:06 +0530

    Getting major number and registering cdev sccessfully

--- scull/char_scull.c
+++ scull/char_scull.c
@@ -4,12 +4,13 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/types.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("KETAN");
 
 /*dev will contain device number within kernel*/
-static dev_t dev_no;
+static dev_t dev_no = 0;
 /*count will be number of devices required*/
 static int count = 1;
 
@@ -45,35 +46,49 @@ struct file_operations scull_fops = {
 	.write   = scull_write,
 };
 
+static void scull_clear(void)
+{
+	pr_alert("Cleaning\n");
+	if(dev){
+		pr_alert("Removing dev\n");
+		cdev_del(&dev->cdev);
+	}
+	pr_alert("Deallocating Number\n");
+	unregister_chrdev_region(dev_no, count);
+        pr_alert("Bye\n");
+}
+
 static int scull_init(void)
 {
 	int ret;
 	int firstminor = 0;
 	int scull_major;
 	/*Name of device driver*/
-	const char *name = "scull\n";
 	/*Request dynamic allocation of major number*/
-	ret = alloc_chrdev_region(&dev_no, firstminor, count, name);
+	ret = alloc_chrdev_region(&dev_no, firstminor, count, "scull");
 	scull_major = MAJOR(dev_no);
 	if (ret <  0)
-		pr_warning("Scull didn't get major number %d \n", scull_major);
+		pr_err("Scull didn't get major number %d \n", scull_major);
 	else
 		pr_alert("We get major number %d \n", scull_major);
 	/*Device registration in scull*/
+	dev = kmalloc(sizeof(struct scull_dev), GFP_KERNEL);
+	if (!dev) {
+		ret = -ENOMEM;
+		goto fail;  /* Make this more graceful */
+	}
 	cdev_init(&dev->cdev, &scull_fops);
 	dev->cdev.owner = THIS_MODULE;
 	dev->cdev.ops   = &scull_fops;
 	ret = cdev_add(&dev->cdev, dev_no, count);
-	if (ret)
-		pr_notice("Error %d adding cdev\n",ret);
+	if (ret){
+		pr_err("Error %d adding cdev\n",ret);
+		goto fail;
+	}
+	pr_alert("Done it\n");
 	return 0;
-}
-
-static void scull_clear(void)
-{
-	cdev_del(&dev->cdev);
-	unregister_chrdev_region(dev_no, count);
-        pr_debug("Bye\n");
+fail:	scull_clear();
+	return ret;
 }
 
 module_init(scull_init);
